425f63e2c4f915eeb6ca214583f07065c395e0f9
DESKTOP-L88LUBN\София <sofiya.fish02@mail.ru>
2025-04-23 15:08:39 +0300

    feat: add sidebar and header components

---
+++ src/pages/OverviewPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from "../components/common/Header.jsx";
+
+const OverviewPage = () => {
+    return (
+        <div className='flex-1 overflow-auto relative z-10'>
+            <Header title='Overview'/>
+        </div>
+    )
+}
+export default OverviewPage
