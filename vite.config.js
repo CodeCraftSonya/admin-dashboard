0a31175778b503273f18aba69466399463387d28
DESKTOP-L88LUBN\София <sofiya.fish02@mail.ru>
2025-04-27 16:00:56 +0300

    fix: change vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/admin-dashboard/',
+  base: process.env.VITE_BASE_PATH || '/admin-dashboard',
   plugins: [react()],
 })
 
